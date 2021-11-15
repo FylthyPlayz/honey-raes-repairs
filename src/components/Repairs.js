568420953bf1ab21eddac38e88662fe037ef2431
FylthyPlayz <jacob_cook_football@yahoo.com>
2021-11-15 16:01:52 -0600

    updated repairs

---
+++ src/components/Repairs.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+
+export const Repairs = () => {
+    const [customers, changeCustomers] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/customers")
+                .then(res => res.json())
+                .then((customerArray) => {
+                    changeCustomers(customerArray)
+                })
+        },
+        []
+    )
+
+    return (
+        <>
+            <h1>Honey Rae's Repair Shop</h1>
+
+            {
+                customers.map(
+                    (customerObj) => {
+                        return <h2>{customerObj.name}</h2>
+                    }
+                )
+            }
+        </>
+    )
+}
